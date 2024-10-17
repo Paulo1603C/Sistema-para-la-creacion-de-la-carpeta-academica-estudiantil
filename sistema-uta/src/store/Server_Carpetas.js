a4f1353b69cf9c101f372bb07f26e0d347eb066f
Paulo Martinez <paulomartinez1999@gmail.com>
2024-10-17 12:11:22 -0500

    cambio en serverCarpetas.js

--- sistema-uta/src/store/Server_Carpetas.js
+++ sistema-uta/src/store/Server_Carpetas.js
@@ -3,92 +3,86 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 import { baseURL } from './config';
-
 export default {
     namespaced: true,
 
     state: {
         listCarpetas: [],
         rutaAnterior: "",  //servira para tener la ruta que queremos actualizar en el servidor
+
+        //
         dataCarpeta: {},
-        isLoading: false,  // Control de carga
-        errorMessage: "",  // Mensaje de error
+
     },
 
     getters: {
         getCarpetas(state) {
             return state.listCarpetas;
         },
-        isLoading(state) {
-            return state.isLoading;
-        },
-        getErrorMessage(state) {
-            return state.errorMessage;
-        }
     },
 
     mutations: {
         llenarlista(state, Carpetas) {
             state.listCarpetas = Carpetas;
         },
+
         setRutaAnterior(state, value) {
             state.rutaAnterior = value;
         },
+
         setCarpeta(state, value) {
             state.dataCarpeta = value;
         },
-        setIsLoading(state, value) {
-            state.isLoading = value;
-        },
-        setErrorMessage(state, value) {
-            state.errorMessage = value;
-        }
+
     },
 
     actions: {
 
         cargarCarpetas: async function ({ commit }, ruta) {
-            commit('setIsLoading', true);  // Iniciar carga
-            commit('setErrorMessage', ""); // Limpiar errores anteriores
-            try {
-                if (ruta) {
+            //console.log('RUTA===>' + ruta.toUpperCase());
+            if (ruta) {
+                try {
                     const items = new FormData();
                     items.append('rutaServidor', ruta.toUpperCase());
 
                     const setting = {
                         method: 'POST',
                         body: items,
-                    };
+                    }
                     const url = `${baseURL}Dir_ArchSelect.php`;
                     const data = await fetch(url, setting);
                     if (data.ok) {
                         const json = await data.text();
+                        //console.log(json);
                         if (json.startsWith('{')) {
                             const jsonData = JSON.parse(json);
                             commit('llenarlista', jsonData);
                         } else {
                             const errorResponse = JSON.parse(json);
                             const errorMessage = errorResponse.error;
-                            commit('setErrorMessage', errorMessage);  // Establecer el error
+                            commit('llenarlista', errorMessage);
                         }
                     }
+                } catch (error) {
+                    console.error('Error en la solicitud:', error);
+                    commit('setErrorCargaCarpetas', 'Error al cargar las carpetas: ' + error.message);
                 }
-            } catch (error) {
-                commit('setErrorMessage', 'Error al cargar las carpetas: ' + error.message);
-            } finally {
-                commit('setIsLoading', false);  // Terminar carga
             }
+
         },
 
         crearCarpeta: async function ({ commit, dispatch }, { datos, path, oldPath }) {
-            commit('setIsLoading', true);  // Iniciar carga
-            commit('setErrorMessage', ""); // Limpiar errores anteriores
+            //console.log('IDEst ' + JSON.stringify(datos));/
             try {
                 const datosCarpeta = new FormData();
                 const rutaC = path + datos.NomEst.toUpperCase() + ' ' + datos.ApeEst.toUpperCase();
                 const aux = rutaC.indexOf('.');
                 if (aux < 0) {
-                    if (datos.ApeEst === '') {
+                    if (datos.ApeEst == '') {
+                        console.log('just folder');
+                        //console.log(path);
+                        //console.log( path + datos.NomEst.toUpperCase());
+                        //console.log(  oldPath.toUpperCase() );
                         datosCarpeta.append('nuevoNombreDirectorio', path + datos.NomEst.toUpperCase());
                     } else {
                         datosCarpeta.append('nuevoNombreDirectorio', path + datos.NomEst.toUpperCase() + ' ' + datos.ApeEst.toUpperCase());
@@ -101,31 +95,36 @@ export default {
                 const setting = {
                     method: 'POST',
                     body: datosCarpeta,
-                };
-                const url = datos.IdEst === 0 ? `${baseURL}crearCarpetas.php` : `${baseURL}actualizarCarpetaServidor.php`;
-
+                }
+                //console.log("ID" + datos.IdEst);
+                var url = "";
+                if (datos.IdEst == 0) {
+                    url = `${baseURL}crearCarpetas.php`;
+                } else {
+                    url = `${baseURL}actualizarCarpetaServidor.php`;
+                }
                 const data = await fetch(url, setting);
                 const json = await data.text();
                 if (json.startsWith('{')) {
                     const jsonData = JSON.parse(json);
+                    console.log('json');
                     dispatch('cargarCarpetas', path);
                 } else {
+                    console.log('Text');
                     dispatch('cargarCarpetas', path);
                 }
             } catch (error) {
-                commit('setErrorMessage', 'Error al crear la carpeta: ' + error.message);
-            } finally {
-                commit('setIsLoading', false);  // Terminar carga
+                console.error('Error en la solicitud:', error);
             }
+
         },
 
         crearSubCarpeta: async function ({ commit, dispatch }, { datos, path, nombre }) {
-            commit('setIsLoading', true);
-            commit('setErrorMessage', "");
             try {
                 const datosCarpeta = new FormData();
                 datosCarpeta.append('nuevoNombreDirectorio', path.toUpperCase() + datos.NomEst.toUpperCase() + ' ' + datos.ApeEst.toUpperCase() + '/' + nombre.toUpperCase());
-                
+                datosCarpeta.append('nuevoNombreDirectorio', path.toUpperCase() + datos.NomEst.toUpperCase() + ' ' + datos.ApeEst.toUpperCase() + '/' + nombre.toUpperCase());
+                console.log('RUTA-> ' + path.toUpperCase() + datos.NomEst.toUpperCase() + ' ' + datos.ApeEst.toUpperCase()+ '/' + nombre.toUpperCase());
                 const setting = {
                     method: 'POST',
                     body: datosCarpeta,
@@ -134,49 +133,50 @@ export default {
                 const data = await fetch(url, setting);
                 const json = await data.text();
                 if (json.startsWith('{')) {
+                    const jsonData = JSON.parse(json);
+                    console.log('json');
                     dispatch('cargarCarpetas', path);
                 } else {
+                    console.log('Text');
                     dispatch('cargarCarpetas', path);
                 }
             } catch (error) {
-                commit('setErrorMessage', 'Error al crear la subcarpeta: ' + error.message);
-            } finally {
-                commit('setIsLoading', false);
+                console.error('Error en la solicitud:', error);
             }
+
         },
 
         crearSubSubCarpeta: async function ({ commit, dispatch }, { datos, nomSubDir ,path, nombre }) {
-            commit('setIsLoading', true);
-            commit('setErrorMessage', "");
             try {
                 const datosCarpeta = new FormData();
-                datosCarpeta.append('nuevoNombreDirectorio', path.toUpperCase() + datos.NomEst.toUpperCase() + ' ' + datos.ApeEst.toUpperCase() + '/' + nomSubDir.toUpperCase() + '/' + nombre.toUpperCase());
-
+                datosCarpeta.append('nuevoNombreDirectorio', path.toUpperCase() + datos.NomEst.toUpperCase() + ' ' + datos.ApeEst.toUpperCase()+'/'+nomSubDir.toUpperCase()+'/'+nombre.toUpperCase());
+                console.log('RUTA-> ' + path.toUpperCase() + datos.NomEst.toUpperCase() + ' ' + datos.ApeEst.toUpperCase()+'/'+nomSubDir.toUpperCase()+'/'+nombre.toUpperCase());
                 const setting = {
                     method: 'POST',
                     body: datosCarpeta,
-                };
+                }
                 const url = `${baseURL}crearCarpetas.php`;
                 const data = await fetch(url, setting);
                 const json = await data.text();
                 if (json.startsWith('{')) {
+                    const jsonData = JSON.parse(json);
+                    console.log('json');
                     dispatch('cargarCarpetas', path);
                 } else {
+                    console.log('Text');
                     dispatch('cargarCarpetas', path);
                 }
             } catch (error) {
-                commit('setErrorMessage', 'Error al crear la sub-subcarpeta: ' + error.message);
-            } finally {
-                commit('setIsLoading', false);
+                console.error('Error en la solicitud:', error);
             }
+
         },
 
         eliminarCarpeta: async function ({ commit, dispatch }, { ruta1, ruta2, tipo }) {
-            commit('setIsLoading', true);
-            commit('setErrorMessage', "");
             try {
+                //console.log('Server'+ruta2.toUpperCase())
                 const datosCarpeta = new FormData();
-                if (tipo === 'Archivo') {
+                if (tipo == 'Archivo') {
                     datosCarpeta.append('rutaServidor', ruta2);
                 } else {
                     datosCarpeta.append('rutaServidor', ruta2.toUpperCase());
@@ -184,51 +184,58 @@ export default {
                 const setting = {
                     method: 'POST',
                     body: datosCarpeta,
-                };
+                }
                 const url = `${baseURL}eliminarCarpetaServidor.php`;
                 const data = await fetch(url, setting);
                 const json = await data.text();
                 if (json.startsWith('{')) {
+                    const jsonData = JSON.parse(json);
                     dispatch('cargarCarpetas', ruta1);
+                    return true;
                 } else {
                     dispatch('cargarCarpetas', ruta1);
+                    return true;
                 }
             } catch (error) {
-                commit('setErrorMessage', 'Error al eliminar la carpeta: ' + error.message);
-            } finally {
-                commit('setIsLoading', false);
+                console.log("Error de eliminción " + error);
             }
         },
 
-        descargarCarpeta: async function ({ commit }, { ruta, nombre }) {
-            commit('setIsLoading', true);
-            commit('setErrorMessage', "");
+        descargarCarpeta: async function ({ commit, dispatch }, { ruta, nombre }) {
             try {
+                console.log('RUTA ' + nombre.toUpperCase());
                 const datosArchivos = new FormData();
                 datosArchivos.append('rutaRemota', ruta + nombre.toUpperCase().trim());
 
                 const setting = {
                     method: 'POST',
                     body: datosArchivos,
-                };
+                }
                 const url = `${baseURL}descargarCarpetas.php`;
-                const response = await fetch(url, setting);
+                const response = await fetch(url, setting)
 
                 if (response.ok) {
                     const arrayBuffer = await response.arrayBuffer();
                     const blob = new Blob([arrayBuffer], { type: 'application/zip' });
+                    // Crear un enlace de descarga y hacer clic en él
+                    console.log(blob);
                     const a = document.createElement('a');
                     a.href = window.URL.createObjectURL(blob); // Crear la URL del Blob localmente
-                    a.download = nombre || `${ruta}`;
+                    if (nombre != '') {
+                        nombre = nombre;
+                    } else {
+                        nombre = `${ruta}`;
+                    }
+                    a.download = nombre; // Nombre del archivo que se guardará localmente
                     document.body.appendChild(a);
                     a.click();
                     window.URL.revokeObjectURL(a.href);
                 }
+
             } catch (error) {
-                commit('setErrorMessage', 'Error al descargar la carpeta: ' + error.message);
-            } finally {
-                commit('setIsLoading', false);
+                console.error('Error en la solicitud:', error);
             }
         },
+
     }
 }
